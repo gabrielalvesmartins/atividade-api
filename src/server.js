66dac899dec9b85add022663aef51fcad7dd8ab4
EA - Gabriel Alves Martins <martins_alves01@educadventista.org>
2026-05-14 11:01:37 -0300

    Fix Prisma import paths and add missing root dependencies

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaMariaDb({
   url: process.env.DATABASE_URL
